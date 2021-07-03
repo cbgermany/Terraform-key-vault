5094ed98eedda243252222ad8e5d339182a3714c
Chris Germany <Chris.Germany@blueyonder.co.uk>
2021-07-03 04:56:11 +0000

    Initial commit

---
+++ main.tf
@@ -0,0 +1,26 @@
+# creates a new key-vault and associated resource group
+resource "azurerm_resource_group" "vault" {
+    name     = var.resource_group
+    location = var.location
+}
+
+resource "azurerm_key_vault" "vault" {
+    name                       = var.name
+    location                   = azurerm_resource_group.vault.location
+    resource_group_name        = azurerm_resource_group.vault.name
+    tenant_id                  = var.tenant_id
+    soft_delete_retention_days = 7
+    sku_name                   = "standard"
+}
+
+resource "azurerm_key_vault_access_policy" "vault" {
+  for_each = var.access_policies
+  key_vault_id = azurerm_key_vault.vault.id
+  tenant_id    = var.tenant_id
+
+  object_id    = each.value.object_id
+
+  key_permissions = each.value.keys
+  secret_permissions = each.value.secrets
+
+}
---
+++ variables.tf
@@ -0,0 +1,32 @@
+# Variables used by this module
+
+variable "resource_group" {
+    description = "Resource Group to hold the Key Vault"
+    type        = string
+}
+
+variable "location" {
+    description = "Location where the Key Vault will be created"
+    type        = string
+}
+
+variable "name" {
+    description = "The name od the key vault"
+    type        = string
+}
+
+variable "tenant_id" {
+    description = "The Tenant Id for the subscription"
+    type        = string
+}
+
+# Map of the access policies
+variable "access_policies" {
+  description = "Map of access policies for the key vault"
+  type = map(object({
+    object_id = string
+    keys      = list(string)
+    secrets   = list(string)
+  }))
+  default = {}
+}
